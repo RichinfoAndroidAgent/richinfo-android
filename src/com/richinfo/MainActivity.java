84707fafcf2450acbc4f5f7b515fb62a67cb61fd
Zach <liujia@richinfo.cn>
2015-07-03 15:43:58 +0800

    first import

---
+++ src/com/richinfo/MainActivity.java
@@ -0,0 +1,11 @@
+package com.richinfo;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
